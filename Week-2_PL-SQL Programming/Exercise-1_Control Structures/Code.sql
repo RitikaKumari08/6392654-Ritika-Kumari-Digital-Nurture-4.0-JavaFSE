f7c88e74f1a2a49feb37a25bf6fc76d1179b2d65
RitikaKumari08 <145172195+RitikaKumari08@users.noreply.github.com>
2025-06-27 22:26:10 +0530

    Update Code.sql

--- Week-2_PL-SQL Programming/Exercise-1_Control Structures/Code.sql
+++ Week-2_PL-SQL Programming/Exercise-1_Control Structures/Code.sql
@@ -47,7 +47,8 @@ SELECT * FROM Customers;
 --View Loans Table
 SELECT * FROM Loans;
 
---Scenario 1:
+--Scenario 1:The bank wants to apply a discount to loan interest rates for customers above 60 years old.
+--Question: Write a PL/SQL block that loops through all customers, checks their age, and if they are above 60, apply a 1% discount to their current loan interest rates.
 DECLARE
     CURSOR cur_customers IS
         SELECT CustomerID, InterestRate
@@ -70,7 +71,9 @@ FROM Customers c
 JOIN Loans l ON c.CustomerID = l.CustomerID
 WHERE c.Age > 60;
 
---Scenario 2:
+
+--Scenario 2:A customer can be promoted to VIP status based on their balance.
+--Question: Write a PL/SQL block that iterates through all customers and sets a flag IsVIP to TRUE for those with a balance over $10,000.
 BEGIN
     FOR rec IN (
         SELECT CustomerID
@@ -87,7 +90,9 @@ SELECT CustomerID, Name, Balance, IsVIP
 FROM Customers
 WHERE IsVIP = 'TRUE';
 
---Scenario 3:
+
+--Scenario 3:The bank wants to send reminders to customers whose loans are due within the next 30 days.
+--Question: Write a PL/SQL block that fetches all loans due in the next 30 days and prints a reminder message for each customer.
 DECLARE
     CURSOR cur_due_loans IS
         SELECT l.CustomerID, l.DueDate, c.Name
