c9207af7d98da1b9ed5dbeb22f846bd647b958eb
RitikaKumari08 <145172195+RitikaKumari08@users.noreply.github.com>
2025-07-10 22:17:32 +0530

    Week-4 Hands-on

---
+++ Week-4_Spring REST using Spring Boot 3/2. spring-rest-handson/REST-Country Web Service/CODE/spring-learn-country-web-service/src/main/java/com/cognizant/spring_learn_country_web_service/SpringLearnCountryWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.spring_learn_country_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnCountryWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnCountryWebServiceApplication.class, args);
+	}
+
+}
---
+++ Week-4_Spring REST using Spring Boot 3/2. spring-rest-handson/REST-Country Web Service/CODE/spring-learn-country-web-service/src/test/java/com/cognizant/spring_learn_country_web_service/SpringLearnCountryWebServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cognizant.spring_learn_country_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(locations="classpath:country.xml")
+class SpringLearnCountryWebServiceApplicationTests {
+
+	//@Test
+	//void contextLoads() {
+	//}
+
+}
